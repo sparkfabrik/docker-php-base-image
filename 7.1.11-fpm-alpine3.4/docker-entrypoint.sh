3a0fbfcdb63782dd75f4f0d9c02e901db29adf83
Paolo Mainardi <paolo.mainardi@sparkfabrik.com>
2017-11-13 16:15:24 +0100

    add 7.1.11-fpm-alpine3.4

---
+++ 7.1.11-fpm-alpine3.4/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+export PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT:-"128M"}
+export PHP_TIMEZONE=${PHP_TIMEZONE:-"Europe/Rome"}
+export PHP_OPCACHE_ENABLE=${PHP_OPCACHE_ENABLE:-1}
+export PHP_OPCACHE_MEMORY=${PHP_OPCACHE_MEMORY:-64}
+
+# Services.
+export MAILHOG_ENABLE=${MAILHOG_ENABLE:-1}
+export MEMCACHED_ENABLE=${MEMCACHED_ENABLE:-0}
+export XDEBUG_ENABLE=${XDEBUG_ENABLE:-0}
+
+# Services configurations.
+export MAILHOG_HOST=${MAILHOG_HOST:-"mail"}
+export MAILHOG_PORT=${MAILHOG_PORT:-1025}
+
+if [ "${MEMCACHED_ENABLE}" = "1" ]; then
+	cp /usr/local/etc/php/conf.disabled/memcached.ini /usr/local/etc/php/conf.d/memcached.ini
+else
+	rm /usr/local/etc/php/conf.d/memcached.ini
+fi
+
+if [ "${MAILHOG_ENABLE}" = "1" ]; then
+	cp /usr/local/etc/php/conf.disabled/mailhog.ini /usr/local/etc/php/conf.d/mailhog.ini
+else
+	rm /usr/local/etc/php/conf.d/mailhog.ini
+fi
+
+if [ "${XDEBUG_ENABLE}" = "1" ]; then
+	cp /usr/local/etc/php/conf.disabled/xdebug.ini /usr/local/etc/php/conf.d/xdebug.ini
+else
+	rm /usr/local/etc/php/conf.d/xdebug.ini
+fi
+
+exec php-fpm
+
+
